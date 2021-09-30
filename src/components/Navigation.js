e1eb16dc14327757a8f4acae350e7444f282ec0a
seongye Park <sungye24@naver.com>
2021-09-30 22:35:25 +0900

    add detail page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function Navigation(){
-    return (<div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+    return (
+    <div>
+        <Link to= "/">Home</Link>
+        <Link to= "/about">About</Link>
     </div>
     );
 }
